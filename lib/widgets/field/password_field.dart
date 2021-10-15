314efde71fddf2c324604fffe8dfb6cee02787dc
Pogryziony <Pogryziony98@interia.pl>
2021-10-16 00:35:25 +0200

    Add password field for user login form

---
+++ lib/widgets/field/password_field.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PasswordField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: const TextStyle(color: Colors.white),
+      cursorColor: Colors.white,
+      obscureText: true,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: 'Password',
+        hintStyle: const TextStyle(color: Colors.white),
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32),
+          borderSide: const BorderSide(color: Colors.white, width: 2),
+        ),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32),
+            borderSide: const BorderSide(color: Colors.white, width: 2)
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32),
+            borderSide: const BorderSide(color: Colors.white, width: 2)
+        ),
+      ),
+    );
+  }
+}
