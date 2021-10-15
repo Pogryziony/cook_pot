ce970879bcbdd8a0b3dbcd4ff66d6ae04c2d1f57
Pogryziony <Pogryziony98@interia.pl>
2021-10-16 00:26:57 +0200

    Add login form field

---
+++ lib/widgets/field/login_field.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class EmailField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: const TextStyle(color: Colors.white),
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: 'Login',
+        hintStyle: const TextStyle(color: Colors.white),
+        focusColor: Colors.white,
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32),
+          borderSide: const BorderSide(color: Colors.white, width: 2),
+        ),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32),
+            borderSide: const BorderSide(color: Colors.white, width: 2)
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32),
+            borderSide: const BorderSide(color: Colors.white, width: 2)
+        ),
+      ),
+    );
+  }
+}
