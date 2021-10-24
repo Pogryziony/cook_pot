80cede17db92c9be1da104886efe8f88fdb49ca1
Pogryziony <Pogryziony98@interia.pl>
2021-10-24 19:51:57 +0200

    Added margin - changed to container

--- lib/widgets/button/login_button.dart
+++ lib/widgets/button/login_button.dart
@@ -3,21 +3,24 @@ import 'package:flutter/material.dart';
 class LoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Colors.orange,
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
-        child: const Text(
-          'Login',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.orange,
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () {},
+          child: const Text(
+            'Login',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
--- lib/widgets/field/login_field.dart
+++ lib/widgets/field/login_field.dart
@@ -3,27 +3,31 @@ import 'package:flutter/material.dart';
 class LoginField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      style: const TextStyle(color: Colors.white),
-      cursorColor: Colors.white,
-      decoration: InputDecoration(
-        contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        hintText: 'Login',
-        hintStyle: const TextStyle(color: Colors.white),
-        focusColor: Colors.white,
-        disabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(32),
-          borderSide: const BorderSide(color: Colors.white, width: 2),
-        ),
-        enabledBorder: OutlineInputBorder(
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: TextFormField(
+        style: const TextStyle(color: Colors.white),
+        cursorColor: Colors.white,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: 'Login',
+          hintStyle: const TextStyle(color: Colors.white),
+          focusColor: Colors.white,
+          disabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(32),
-            borderSide: const BorderSide(color: Colors.white, width: 2)
-        ),
-        focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(32),
-            borderSide: const BorderSide(color: Colors.white, width: 2)
+            borderSide: const BorderSide(color: Colors.white, width: 2),
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32),
+              borderSide: const BorderSide(color: Colors.white, width: 2)
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32),
+              borderSide: const BorderSide(color: Colors.white, width: 2)
+          ),
         ),
       ),
     );
+
   }
 }
