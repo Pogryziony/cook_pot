e5c053cc45ff44d6358eda4e394021a00ef7bfd9
Pogryziony <Pogryziony98@interia.pl>
2021-10-24 19:52:36 +0200

    Add login screen

--- lib/core/auth/login/login_screen.dart
+++ lib/core/auth/login/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cook_pot/widgets/button/login_button.dart';
 import 'package:cook_pot/widgets/field/login_field.dart';
+import 'package:cook_pot/widgets/field/password_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,12 +15,30 @@ class _LoginScreenState extends State<LoginScreen> {
       resizeToAvoidBottomInset: false,
       body: Center(
         child: Container(
-          child: LoginField(),
-          //decoration: const BoxDecoration(
-            // image: DecorationImage(
-            //   image: AssetImage(''),
-            // ),
-         // ),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            margin: const EdgeInsets.only(top: 70),
+            padding: const EdgeInsets.all(36.0),
+            child: ListView(
+              children: <Widget>[
+                SizedBox(
+                  child: Image.asset(
+                    'assets/images/logo/cook_pot_logo_150px_transparent.png',
+                    fit: BoxFit.fitHeight,
+                    height: 200, // set your height
+                  ),
+                ),
+                LoginField(),
+                PasswordField(),
+                LoginButton(),
+              ],
+            ),
+          ),
         ),
       ),
     );
