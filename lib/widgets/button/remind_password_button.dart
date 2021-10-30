a4670fb026b32d10d82a73d09cefcf312f3557f3
Pogryziony <Pogryziony98@interia.pl>
2021-10-30 15:45:22 +0200

    Add button for password remind

--- lib/widgets/button/remind_password_button.dart
+++ lib/widgets/button/remind_password_button.dart
@@ -4,21 +4,18 @@ class RemindPasswordButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10),
-      width: 20,
       child: Material(
         elevation: 5.0,
         borderRadius: BorderRadius.circular(30.0),
         color: Colors.orange,
         child: MaterialButton(
           minWidth: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           onPressed: () {},
           child: const Text(
-            'Remind password',
+            'Forgot password',
             textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 18,
+              fontSize: 16,
               color: Colors.white,
               fontWeight: FontWeight.bold,
             ),
