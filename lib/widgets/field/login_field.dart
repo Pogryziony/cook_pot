e0d7eba184bae74b263d4398dbb97249527f29bd
Pogryziony <Pogryziony98@interia.pl>
2021-11-04 21:06:31 +0100

    Visual changes

--- lib/widgets/field/login_field.dart
+++ lib/widgets/field/login_field.dart
@@ -4,7 +4,7 @@ class LoginField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(5),
       child: TextFormField(
         style: const TextStyle(color: Colors.white),
         cursorColor: Colors.white,
--- lib/widgets/field/password_field.dart
+++ lib/widgets/field/password_field.dart
@@ -1,14 +1,16 @@
+import 'package:cook_pot/utils/helpers/validate_password.dart';
 import 'package:flutter/material.dart';
 
 class PasswordField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(5),
       child: TextFormField(
         style: const TextStyle(color: Colors.white),
         cursorColor: Colors.white,
         obscureText: true,
+        validator: validatePassword,
         decoration: InputDecoration(
           contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           hintText: 'Password',
