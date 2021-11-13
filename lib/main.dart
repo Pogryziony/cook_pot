3519a46b79e89707fadc1fd66bea3e2833e1ff44
Pogryziony <Pogryziony98@interia.pl>
2021-11-13 10:44:39 +0100

    Changes for firebase implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,45 @@
 import 'package:cook_pot/core/first_steps/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:cook_pot/core/auth/login/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseApp app = await Firebase.initializeApp();
   runApp(MyApp());
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (BuildContext context, AsyncSnapshot<Object?> snapshot) {
+        if (snapshot.hasError) {
+          return InitializationError();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
+
+class InitializationError extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +47,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cook Pot',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
