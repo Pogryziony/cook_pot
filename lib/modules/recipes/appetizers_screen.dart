435b18d5defd5aff023382e5022537c67402a748
Pogryziony <Pogryziony98@interia.pl>
2021-12-13 09:24:44 +0100

    Change in appetizer screen

--- lib/modules/recipes/appetizers_screen.dart
+++ lib/modules/recipes/appetizers_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cook_pot/models/recipe.dart';
 import 'package:cook_pot/modules/recipes/appetizers_bloc.dart';
+import 'package:cook_pot/widgets/card/recipe_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,36 +10,42 @@ class AppetizersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Appetizer menu'),
-        backgroundColor: Colors.lightGreen,
-      ),
-      body: BlocBuilder<AppetizersBloc, AppetizersState>(
-        builder: (BuildContext context, AppetizersState state) {
-          if (state is AppetizersLoadingState) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is AppetizersLoadErrorState) {
-            return Center(
-              child: Text(state.error),
-            );
-          } else if (state is AppetizersLoadedState) {
-            return _appetizerList(state.appetizers);
-          }
-          return Container();
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Appetizer menu'),
+          backgroundColor: Colors.lightGreen,
+        ),
+        body: BlocBuilder<AppetizersBloc, AppetizersState>(
+          builder: (BuildContext context, AppetizersState state) {
+            if (state is AppetizersLoadingState) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is AppetizersLoadErrorState) {
+              return Center(
+                child: Text(state.error),
+              );
+            } else if (state is AppetizersLoadedState) {
+              return _appetizerList(state.appetizers);
+            }
+            return Container();
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ));
   }
 
   Widget _appetizerList(List<Recipe> appetizers) {
     return ListView.builder(
         itemCount: appetizers.length,
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(appetizers[index].name!),
-            subtitle: Text(appetizers[index].preparationTime.toString()),
+          return RecipeCard(
+            recipeName: appetizers[index].name!.toString(),
+            assetPath: appetizers[index].image!,
+            preparationTime: 'Preparation time: ' +
+                appetizers[index].preparationTime.toString(),
+            callbackFunction: () => {},
           );
         });
   }
