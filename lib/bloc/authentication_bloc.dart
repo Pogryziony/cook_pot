62c3c69f0551ce018b0daee3dcecd780bae7c868
Pogryziony <Pogryziony98@interia.pl>
2021-12-13 09:14:07 +0100

    Export auth and login logic to lib

---
+++ lib/bloc/authentication_bloc.dart
@@ -0,0 +1,6 @@
+library authentication_bloc;
+
+export 'authentication_bloc/authentication_bloc.dart';
+export 'authentication_bloc/authentication_event.dart';
+export 'authentication_bloc/authentication_state.dart';
+
