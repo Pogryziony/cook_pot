374aef7938174c97498e3beb681c2fc1f79b6d99
Pogryziony <Pogryziony98@interia.pl>
2022-01-02 18:50:52 +0100

    Change in certificates - dev mode

--- lib/modules/recipes/appetizers/appetizers_screen.dart
+++ lib/modules/recipes/appetizers/appetizers_screen.dart
@@ -1,18 +1,45 @@
 import 'package:cook_pot/models/recipe.dart';
 import 'package:cook_pot/modules/recipes/appetizers_bloc.dart';
 import 'package:cook_pot/widgets/card/recipe_card.dart';
+import 'package:cook_pot/widgets/filter_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'appetizers_bloc.dart';
 
-class AppetizersScreen extends StatelessWidget {
+class AppetizersScreen extends StatefulWidget {
+  @override
+  _AppetizersScreenState createState() => _AppetizersScreenState();
+}
+
+class _AppetizersScreenState extends State<AppetizersScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Appetizer menu'),
           backgroundColor: Colors.lightGreen,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              tooltip: 'Open search module',
+              onPressed: () {
+                // handle the press
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.dehaze_outlined),
+              tooltip: 'Open filter module',
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return FilterList();
+                    });
+              },
+            ),
+          ],
         ),
         body: BlocBuilder<AppetizersBloc, AppetizersState>(
           builder: (BuildContext context, AppetizersState state) {
