31adde7e07938c9d1ab62c326fdd8297d0b55df5
Pogryziony <Pogryziony98@interia.pl>
2022-01-12 20:33:07 +0100

    Add an appetizer detail screen

---
+++ lib/modules/recipes/appetizers/appetizer_detail_screen.dart
@@ -0,0 +1,121 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_icons/flutter_icons.dart';
+
+class AppetizerDetailScreen extends StatefulWidget {
+  var index;
+  var name;
+  var image;
+  var preparationTime;
+  var difficulty;
+  var ingredients;
+  var preparationSteps;
+  var portions;
+
+  AppetizerDetailScreen(
+      {this.index,
+      this.name,
+      this.image,
+      this.preparationTime,
+      this.difficulty,
+      this.ingredients,
+      this.preparationSteps,
+        this.portions,
+      });
+
+  @override
+  _AppetizerDetailScreenState createState() => _AppetizerDetailScreenState();
+}
+
+class _AppetizerDetailScreenState extends State<AppetizerDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.name),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              child: Image(
+                image: NetworkImage(widget.image),
+                fit: BoxFit.fill,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 10)),
+            Center(
+              child: Text(
+                widget.name,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(children: [
+                  chooseDifficultyIcon(widget.difficulty),
+                  Padding(padding: EdgeInsets.only(left: 10)),
+                  Text(widget.difficulty,
+                      style:
+                      TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
+
+                ],),
+                Padding(padding: EdgeInsets.only(left: 10)),
+                Row(
+                  children: [
+                    Icon(Icons.access_time),
+                    Padding(padding: EdgeInsets.only(left: 10)),
+
+                    Text(widget.preparationTime+' min',
+                        style:
+                        TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.only(left: 10)),
+
+                Row(
+                  children: [
+                    Icon(Icons.person),
+                    Padding(padding: EdgeInsets.only(left: 10)),
+
+                    Text(widget.portions,
+                        style:
+                        TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+chooseDifficultyIcon(difficulty) {
+  if (difficulty == "Easy") {
+    return Icon(
+      MaterialCommunityIcons.signal_cellular_1,
+      size: 36,
+    );
+  } else if (difficulty == "Medium") {
+    return Icon(MaterialCommunityIcons.signal_cellular_2, size: 36);
+  } else if (difficulty == 'Hard')
+    return Icon(MaterialCommunityIcons.signal_cellular_3, size: 36);
+}
