3e51d4ad002abb2dcfa5d16708a74772b0859ebc
Pogryziony <Pogryziony98@interia.pl>
2022-02-06 20:56:44 +0100

    Remove method for filtering. Add new recipe method changes

--- lib/utils/services/firestore_service.dart
+++ lib/utils/services/firestore_service.dart
@@ -6,46 +6,30 @@ class FirestoreService {
 
   Future<List<Recipe>> getRecipes() {
     final CollectionReference<Map<String, dynamic>> reference =
-    _databaseInstance.collection('recipes');
-    return reference.get().then((
-        QuerySnapshot<Map<String, dynamic>> snapshot) =>
-        snapshot
-            .docs
+        _databaseInstance.collection('recipes');
+    return reference.get().then(
+        (QuerySnapshot<Map<String, dynamic>> snapshot) => snapshot.docs
             .map((QueryDocumentSnapshot<Map<String, dynamic>> doc) =>
-            Recipe.fromFirestore(doc))
+                Recipe.fromFirestore(doc))
             .toList());
   }
 
   Future<List<Recipe>> getRecipesFromCategory(String category) {
-    final Query<Map<String, dynamic>> reference =
-    _databaseInstance.collection('recipes').where(
-        'type', isEqualTo: '$category');
-    return reference.get().then((
-        QuerySnapshot<Map<String, dynamic>> snapshot) =>
-        snapshot
-            .docs
-            .map((QueryDocumentSnapshot<Map<String, dynamic>> doc) =>
-            Recipe.fromFirestore(doc))
-            .toList());
-  }
-
-  Future<List<Recipe>> getRecipesFromCategoryWithFilters(String category) {
     final Query<Map<String, dynamic>> reference = _databaseInstance
         .collection('recipes')
-        .where('type', isEqualTo: '$category')
-        .where('difficulty', isEqualTo: 'Easy'); //todo dodać filtrowanie
-    return reference.get().then((
-        QuerySnapshot<Map<String, dynamic>> snapshot) =>
-        snapshot
-            .docs
+        .where('type', isEqualTo: '$category');
+    return reference.get().then(
+        (QuerySnapshot<Map<String, dynamic>> snapshot) => snapshot.docs
             .map((QueryDocumentSnapshot<Map<String, dynamic>> doc) =>
-            Recipe.fromFirestore(doc))
+                Recipe.fromFirestore(doc))
             .toList());
   }
 
   Future<void> addNewRecipe(Recipe recipe) {
     var reference = _databaseInstance.collection('recipes');
     return reference
-        .add(recipe.toDatabaseJson()).catchError((error) =>print(error));
+        .add(recipe.toDatabaseJson())
+        // ignore: return_of_invalid_type_from_catch_error
+        .catchError((error) => print(error));
   }
 }
