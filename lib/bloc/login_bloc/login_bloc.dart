b95009e7589e37796e92aef0ac91232a691c2aae
Pogryziony <Pogryziony98@interia.pl>
2022-02-20 13:50:31 +0100

    Change in login pressed method, changed service to repository

--- lib/bloc/login_bloc/login_bloc.dart
+++ lib/bloc/login_bloc/login_bloc.dart
@@ -5,12 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cook_pot/bloc/authentication_bloc.dart';
 import 'package:cook_pot/bloc/login_bloc.dart';
 import 'package:cook_pot/repository/user_repository.dart';
-import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
-
-
-import 'login_event.dart';
-import 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final UserRepository userRepository;
@@ -26,21 +20,24 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     if (event is LoginButtonPressed) {
       yield* _loginPressed(event);
     }
+    if (event is LoggedOut) {
+      yield LoginInitial();
+    }
   }
 
   Stream<LoginState> _loginPressed(LoginEvent event) async* {
     yield LoginLoading();
     try {
-      final success = await userRepository.authenticate(
-          event.email, event.password);
-      
+      final success =
+          await userRepository.authenticate(event.email, event.password);
+
       if (success) {
         authenticationBloc.add(const LoggedIn());
         yield LoginPassed();
-      }  else{
+      } else {
         yield LoginFailure();
       }
-    } on FirebaseException{
+    } on FirebaseException {
       yield LoginFailure();
     }
   }
